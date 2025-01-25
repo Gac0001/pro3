483d5545c97416a3334dd7a4f68138d63f6c3e26
Gac <gabinascalemba@gmail.com>
2025-01-25 02:53:12 +0100

    "finalisation de filtrage de tous les projets de la galerie par categorie"

--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -9,6 +9,12 @@ function getCategories(){
    let btnTous=document.createElement('button')
    btnTous.setAttribute('class', 'btnTous')
    btnTous.innerText='Tous';
+   btnTous.dataset.id=0;
+
+   btnTous.addEventListener('click', function(){
+    document.querySelector('.gallery').innerHTML='';
+    getWorks(projets)
+   })
 
    //cree une div qui va contenir le bouton 'Tous'
    let fltrAll=document.createElement('div');
@@ -28,26 +34,37 @@ function getCategories(){
    for (let ca of categories) {
        //creation du button pour chaque elt de filtres
        let btnfltr=document.createElement('button');
-       btnfltr.setAttribute('class', ca.name)
+       btnfltr.setAttribute('class', 'btnFGal')
 
        //le texte à afficher pour chaque bouton
        btnfltr.textContent = ca.name;
+       btnfltr.dataset.id=ca.id
+
+
+    btnfltr.addEventListener('click', function () {
+    const categoryId = parseInt(btnfltr.dataset.id); // Récupère l'ID de la catégorie
+    const filtreProjetsId = projets.filter(function (projet) {
+            return projet.categoryId === categoryId; // Filtre les projets par catégorie
+    });
+        getWorks(filtreProjetsId); // Met à jour la galerie avec les projets filtrés
+});
 
        //affectation des tous les boutons à la div container
        fltrSpecifk.appendChild(btnfltr)
 
        //affectation de la div container à la div filtre
        filtre.append(fltrSpecifk);
+
    }
 
    console.log(fltrSpecifk)
    console.log(categories);
 
 }
-getCategories()
+
 
 //recuperation de categories
-document.querySelector('.gallery').innerHTML='';
+
 // va chercher les projets 
 let resProjets = await fetch("http://localhost:5678/api/works");
 // les transforme en json
@@ -55,9 +72,10 @@ const projets = await resProjets.json();
 // va chercher la div "gallery" dans le DOM
 console.log(projets);
 
-export function getWorks(){
+export function getWorks(p_pro){
+    document.querySelector('.gallery').innerHTML='';
     const rootGal = document.querySelector(".gallery")
-    for (let pro of projets) {
+    for (let pro of p_pro) {
         // pour chaque projet
         // créer une balise figure, figcaption, img
         let figure= document.createElement('figure');
@@ -71,21 +89,22 @@ export function getWorks(){
         figure.appendChild(image);
         figure.appendChild(title);
         rootGal.appendChild(figure);
+
     }
 }
-
-getWorks()
+getCategories()
+getWorks(projets)
 
 //filtrage de bouton objets
-const boutonObjt=document.querySelector('.fltrSpecifk .Objets')
+/*const boutonObjt=document.querySelector('.fltrSpecifk .Objets')
 boutonObjt.addEventListener('click', function(){
     const filtreProjetsId=projets.filter(function(projet){
     return projet.categoryId==1;
     })
-    
+    getWorks(filtreProjetsId)
     console.log(filtreProjetsId)
 
-})
+})*/
 
 
 
